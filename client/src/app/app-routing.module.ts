f1cf43fe62b26c5b57d649887311c842581b90ab
Nicolo' Abatecola <nicoloantoninoabatecola@gmail.com>
2021-06-16 20:59:48 +0200

    update

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  { path: 'app-home-page', component: HomePageComponent },
+  { path: '', redirectTo: '/app-home-page', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
